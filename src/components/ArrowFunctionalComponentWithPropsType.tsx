fefa35e95f02e60477505582f7385cdcfb482d03
Anastassios Siagkris <asiagris2011@gmail.com>
2025-05-21 18:07:54 +0300

    fix: fixed typo in code

--- src/components/ArrowFunctionalComponentWithPropsType.tsx
+++ src/components/ArrowFunctionalComponentWithPropsType.tsx
@@ -1,30 +1,34 @@
 // type Props = {
-//     title: string;
-//     description: string;
+//   title: string;
+//   description: string;
 // }
-
 
 type A = {
     title: string;
 }
-type B = {
+type B ={
     description: string;
 }
-
 type Props = A & B;
 
+
 // interface Props {
-//     title: string;
+//   title: string;
+// }
+//
+// interface Props {
+//   description: string;
 // }
 
 
-const ArrowFunctionalComponentWithPropsType = ({title, description}:Props) => {
-return(
-    <>
-        <h1 className="text-center mt-12 text-xl font-bold">{title}</h1>
-        <p className="text-center text-grey-800">{description}</p>
-    </>
-)
+const ArrowFunctionalComponentWithPropsType = ({title, description}: Props) => {
+    return (
+        <>
+            <h1 className="text-center text-xl font-bold mt-12">{title}</h1>;
+            <p className="text-center text-gray-800">{description}</p>
+        </>
+    )
+
 }
 
 export default ArrowFunctionalComponentWithPropsType;
